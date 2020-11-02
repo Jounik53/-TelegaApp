6bc45727fe3a28dfe1896df037baf384f140390c
Jounik <Joun_aleks@mail.ru>
2020-11-02 14:50:26 +0300

    Added timer

--- TelegramApp/Program.cs
+++ TelegramApp/Program.cs
@@ -7,6 +7,7 @@ using TLSharp.Core;
 using System.Diagnostics;
 using System.Configuration;
 using System.Collections.Specialized;
+using System.Timers;
 
 namespace TelegramApp
 {
@@ -19,9 +20,14 @@ namespace TelegramApp
         private static int TargetUserId { get; set; }
         private static string Message { get; set; }
 
+        private static TelegramClient client { get; set; }
+        private static TLUser targetUser { get; set; }
+
         public static TelegaService _telegaService;
         public static ConfigService _configService;
-        
+
+        private static Timer aTimer;
+        private static int interval = 10800000;
 
         static async Task Main(string[] args)
         {
@@ -34,18 +40,14 @@ namespace TelegramApp
             GetConfig();
 
             //Initializing and getting a client
-            var client = await Initialization();
+            client = await Initialization();
 
             //Getting target user
-            var targetUser = await GetTargetUser(client);
+            targetUser = await GetTargetUser(client);
 
-            var result = await _telegaService.SendMessage(client, targetUser, Message);
+            Console.WriteLine("Start send by timer: ");
+            SetTimer();
 
-            if (result)
-            {
-                Console.WriteLine("Sent");
-            }
-            
         }
 
         #region Initialization and Get client
@@ -118,5 +120,29 @@ namespace TelegramApp
             }
         }
 
+        private static async Task SendMessageByInterval()
+        {
+            var result = await _telegaService.SendMessage(client, targetUser, Message);
+
+            if (result)
+            {
+                Console.WriteLine("Sent");
+            }
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(interval);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            SendMessageByInterval().GetAwaiter().GetResult();
+        }
     }
 }
